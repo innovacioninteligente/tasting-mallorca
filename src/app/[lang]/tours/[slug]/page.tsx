8b8760919c34719efb0b0c2e4d494b1401e059e3
Andrés Gómez <innovacioninteligente.ia@gmail.com>
2025-11-05 09:17:54 +0000

    Quedó en blanco la web, no se renderiza nada en el DOM:

--- src/app/[lang]/tours/[slug]/page.tsx
+++ src/app/[lang]/tours/[slug]/page.tsx
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import { notFound } from 'next/navigation';
 import { Locale } from '@/dictionaries/config';
 import { getDictionary } from '@/dictionaries/get-dictionary';
