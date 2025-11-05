a3b578f2cadee08cb02e75d4cf10a378470bdd74
Andrés Gómez <innovacioninteligente.ia@gmail.com>
2025-11-05 10:10:31 +0000

    PErfecto, Gemini. Que si le damos dinámismo a este grid, que no quede un

--- src/components/home/gallery-section.tsx
+++ src/components/home/gallery-section.tsx
@@ -4,17 +4,18 @@ import Image from 'next/image';
 import { Camera } from 'lucide-react';
 
 const galleryImages = [
-  { src: 'https://picsum.photos/seed/gallery1/500/800', hint: 'happy couple travel' },
-  { src: 'https://picsum.photos/seed/gallery2/600/400', hint: 'woman travel backpack' },
-  { src: 'https://picsum.photos/seed/gallery3/500/600', hint: 'hiker mountain view' },
-  { src: 'https://picsum.photos/seed/gallery4/600/400', hint: 'traveler city skyline' },
-  { src: 'https://picsum.photos/seed/gallery5/500/700', hint: 'tropical huts water' },
-  { src: 'https://picsum.photos/seed/gallery6/600/400', hint: 'woman pink dress cliff' },
-  { src: 'https://picsum.photos/seed/gallery7/500/600', hint: 'happy man beach' },
-  { src: 'https://picsum.photos/seed/gallery8/600/800', hint: 'family airport travel' },
-  { src: 'https://picsum.photos/seed/gallery9/500/500', hint: 'hiker looking at cliffs' },
-  { src: 'https://picsum.photos/seed/gallery10/600/900', hint: 'woman yellow dress beach' },
-  { src: 'https://picsum.photos/seed/gallery11/500/700', hint: 'couple walking city' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07636-Mejorado-NR.jpg?alt=media&token=083f05f7-cddb-498a-a044-056ee1834adc', hint: 'happy couple travel' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07651-Mejorado-NR.jpg?alt=media&token=e14dbbb0-7221-4bc4-b47a-8b01915e16b1', hint: 'woman travel backpack' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07689-Mejorado-NR.jpg?alt=media&token=4ed4a450-a529-4e29-80a9-c74962bfc760', hint: 'hiker mountain view' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07697-Mejorado-NR.jpg?alt=media&token=59d167ee-edc9-4021-ba8a-bcfbe6342d1c', hint: 'traveler city skyline' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07701-Mejorado-NR.jpg?alt=media&token=ad6a33ad-3b8e-48bb-bb27-b745f3ed03d4', hint: 'tropical huts water' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2Farta.webp?alt=media&token=d0bd894c-736e-4f72-8f4c-0506ce26e4f0', hint: 'woman pink dress cliff' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07715-Mejorado-NR.jpg?alt=media&token=9c653a7e-ed1d-41e9-8bf6-19f5b554d1b4', hint: 'happy man beach' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07923-Mejorado-NR.jpg?alt=media&token=3220a405-18af-4354-8e2e-1d6138137922', hint: 'family airport travel' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07927-Mejorado-NR.jpg?alt=media&token=3cd8ed5c-69a8-4268-b73c-7948dc6eaa34', hint: 'hiker looking at cliffs' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC08000-Mejorado-NR.jpg?alt=media&token=49f4cd5e-6528-4c41-8438-e76afec57e6b', hint: 'woman yellow dress beach' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07842-Mejorado-NR.jpg?alt=media&token=1fda44e7-fd51-456a-8a2a-abd405c91613', hint: 'couple walking city' },
+  { src: 'https://firebasestorage.googleapis.com/v0/b/amparo-aesthetics.firebasestorage.app/o/tasting-mallorca%2Fimages%2FDSC07852-Mejorado-NR.jpg?alt=media&token=998eb000-2ed9-410b-b9e1-c56aa67965b0', hint: 'couple walking city' },
 ];
 
 export function GallerySection() {
