458806db1f7a2607d3585d43e16019ef3a6b02b4
Andrés Gómez <innovacioninteligente.ia@gmail.com>
2025-11-06 11:19:45 +0000

    Es una labor bastante tediosa y no puedo pasarte la clave por aquí, ¿QUé

--- src/firebase/server/config.ts
+++ src/firebase/server/config.ts
@@ -1,8 +1,24 @@
 import { initializeApp, getApp, getApps, cert } from 'firebase-admin/app';
 
+// This is a special case for the dev environment
+// In a deployed environment, you would use environment variables
+// that are properly formatted.
+let serviceAccount: any;
+try {
+  serviceAccount = require('../../../service-account.json');
+} catch (e) {
+  // We ignore this error, as it will only happen in a deployed environment
+}
+
 const createFirebaseAdminApp = () => {
   if (getApps().length > 0) {
     return getApp();
+  }
+
+  if (serviceAccount) {
+     return initializeApp({
+      credential: cert(serviceAccount)
+    });
   }
 
   // Parse the service account key from the environment variable.
@@ -12,10 +28,10 @@ const createFirebaseAdminApp = () => {
   }
 
   try {
-    const serviceAccount = JSON.parse(serviceAccountString);
+    const parsedServiceAccount = JSON.parse(serviceAccountString);
 
     return initializeApp({
-      credential: cert(serviceAccount)
+      credential: cert(parsedServiceAccount)
     });
   } catch (error) {
     console.error('Failed to parse Firebase service account JSON.', error);
